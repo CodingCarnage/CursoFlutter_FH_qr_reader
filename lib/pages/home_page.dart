9545333e242a4f482ab97d171653864fd5df916b
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-21 17:28:42 -0600

    Se creo un AppBar, FloatingActionButton y BottomNavigationBar.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_reader/widgets/custom_navigationbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Historial'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: () {},
+          )
+        ],
+      ),
       body: Center(
         child: Text('Home Page'),
       ),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: CustomNavigationBar(),
     );
   }
 }
