dc0f3a8292a6c772cb6a85656647fdf5426a737d
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-21 20:51:29 -0600

    Se agrego el tema para la aplicación.

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,32 @@ import 'package:flutter/material.dart';
 
 import 'package:qr_reader/pages/home_page.dart';
 import 'package:qr_reader/pages/mapa_page.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'QR Reader',
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 255, 152, 0),
+        primaryColorDark: Color.fromARGB(255, 245, 124, 0),
+        primaryColorLight: Color.fromARGB(255, 255, 224, 178),
+        accentColor: Color.fromARGB(255, 76, 175, 80),
+        // Cambia el color del Texto que este sobre mi PrimaryColor a blanco.
+        primaryTextTheme: TextTheme(headline6: TextStyle(color: Colors.white)),
+        // Cambia el color de los iconos que esten sobre mi PrimaryColor a blanco.
+        primaryIconTheme: IconThemeData(color: Colors.white),
+        // Cambia el color de los iconos del status bar a blanco.
+        appBarTheme: AppBarTheme(brightness: Brightness.dark),
+        fontFamily: 'Poppins',
+      ),
       initialRoute: 'home',
       routes: {
-        'home' : ( _ ) => HomePage(),
-        'mapa' : ( _ ) => MapaPage(),
+        'home': (_) => HomePage(),
+        'mapa': (_) => MapaPage(),
       },
     );
   }
