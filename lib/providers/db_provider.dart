507877e4847519b78f62e36fe7aae86c9435dca8
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-26 12:41:02 -0600

    Se creo el provider de la base de datos al igual que una función para inicializarla.

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:qr_reader/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -6,15 +11,37 @@ class DBProvider {
   DBProvider._();
 
   Future<Database> get database async {
-    if(_database != null)
-      return _database;
-    
+    if (_database != null) return _database;
+
     _database = await initDB();
 
     return _database;
   }
 
   Future<Database> initDB() async {
+    // Obtener e path de la base de datos
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final String path = join(documentsDirectory.path, 'ScansDB.db');
+    print(path);
+
+    // Crear base de datos
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int verison) async {
+        await db.execute('''
+          CREATE TABLE Scans(
+            id INTEGER PRIMARY KEY,
+            tipo TEXT,
+            valor TEXT
+          )
+        ''');
+      }
+    );
+  }
+
+  newScanRaw(ScanModel scanModel) async {
 
   }
 }
