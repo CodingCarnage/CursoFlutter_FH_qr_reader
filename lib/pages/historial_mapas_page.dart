e3f807a4444d016625276713e1ce8d6dd9c499a3
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 15:37:18 -0600

    Ya se puede visualizar los datos directamente de mi base de datos.

--- lib/pages/historial_mapas_page.dart
+++ lib/pages/historial_mapas_page.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/providers/scan_list_provider.dart';
 
 class HistorialMapasPage extends StatelessWidget {
   const HistorialMapasPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final ScanListProvider scanListProvider = Provider.of<ScanListProvider>(context);
+    final scans = scanListProvider.scans;
+
     return ListView.builder(
-      itemCount: 10,
+      itemCount: scans.length,
       itemBuilder: (_, int i) => ListTile(
-        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-        title: Text('https://www.google.com'),
-        subtitle: Text('ID: 1'),
+        leading: Icon(Icons.location_on, color: Theme.of(context).primaryColor),
+        title: Text(scans[i].valor),
+        subtitle: Text('Id: ${scans[i].id.toString()}'),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
         onTap: () => print('Hacer algo'),
       ),
