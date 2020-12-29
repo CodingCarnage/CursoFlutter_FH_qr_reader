5c1d9a9da47f3ffaca38b28eb39a302abaface7e
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 22:46:49 -0600

    Se cambio por un StatefulWidget, al igual se agregaron las funciones de Google Maps.

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,20 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:qr_reader/models/scan_model.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
   const MapaPage({Key key}) : super(key: key);
+
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
     final ScanModel scanModel = ModalRoute.of(context).settings.arguments;
 
+    final CameraPosition puntoIinical = CameraPosition(
+      target: scanModel.getLatLng(),
+      zoom: 17,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(
-        child: Text(scanModel.valor),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: puntoIinical,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
