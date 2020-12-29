80ae925550cfc001c73ca65d553e2d7c2e275c90
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 21:15:27 -0600

    Se agrego un AppBar.

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_reader/models/scan_model.dart';
+
 class MapaPage extends StatelessWidget {
   const MapaPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final ScanModel scanModel = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Mapa'),
+      ),
       body: Center(
-        child: Text('Mapa Page'),
+        child: Text(scanModel.valor),
       ),
     );
   }
