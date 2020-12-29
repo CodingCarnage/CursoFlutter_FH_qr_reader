f25801286c26763fa3c61183561d6f8d1e2c1011
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-29 15:32:04 -0600

    Se cambio la importación de cupertino a material.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:qr_reader/models/scan_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
