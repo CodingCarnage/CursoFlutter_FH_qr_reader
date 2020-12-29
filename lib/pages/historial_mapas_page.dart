c76c2dcc9483d27760e3374a132d6396d7570af7
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 19:50:55 -0600

    Poder borrar un registro unico mediante un slide.

--- lib/pages/historial_mapas_page.dart
+++ lib/pages/historial_mapas_page.dart
@@ -13,12 +13,23 @@ class HistorialMapasPage extends StatelessWidget {
 
     return ListView.builder(
       itemCount: scans.length,
-      itemBuilder: (_, int i) => ListTile(
-        leading: Icon(Icons.location_on, color: Theme.of(context).primaryColor),
-        title: Text(scans[i].valor),
-        subtitle: Text('Id: ${scans[i].id.toString()}'),
-        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-        onTap: () => print('Hacer algo'),
+      itemBuilder: (_, int i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          padding: const EdgeInsets.only(left: 5.0),
+          color: Colors.red,
+          alignment: Alignment.centerLeft,
+          child: const Icon(Icons.delete, size: 30.0, color: Colors.white),
+        ),
+        onDismissed: (DismissDirection direction) => Provider.of<ScanListProvider>(context, listen: false).deleteByIdScan(scans[i].id),
+        direction: DismissDirection.startToEnd,
+        child: ListTile(
+          leading: Icon(Icons.location_on, color: Theme.of(context).primaryColor),
+          title: Text(scans[i].valor),
+          subtitle: Text('Id: ${scans[i].id.toString()}'),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print('Hacer algo'),
+        ),
       ),
     );
   }
