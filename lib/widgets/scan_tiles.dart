283bc105d055f0ee7e50cc92a30c87fb980e80b9
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 20:34:36 -0600

    Se creo un widget personalizado para optimizar 2 paginas, direcciones e historial_mapas.

---
+++ lib/widgets/scan_tiles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/providers/scan_list_provider.dart';
+
+class ScanTiles extends StatelessWidget {
+  const ScanTiles({Key key, @required this.tipo}) : super(key: key);
+
+  final String tipo;
+
+  @override
+  Widget build(BuildContext context) {
+    final ScanListProvider scanListProvider =
+        Provider.of<ScanListProvider>(context);
+    final scans = scanListProvider.scans;
+
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder: (_, int i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          padding: const EdgeInsets.only(left: 5.0),
+          color: Colors.red,
+          alignment: Alignment.centerLeft,
+          child: const Icon(Icons.delete, size: 30.0, color: Colors.white),
+        ),
+        onDismissed: (DismissDirection direction) =>
+            Provider.of<ScanListProvider>(context, listen: false)
+                .deleteByIdScan(scans[i].id),
+        direction: DismissDirection.startToEnd,
+        child: ListTile(
+          leading: Icon(
+            this.tipo == 'http' ? Icons.http : Icons.location_on,
+            color: Theme.of(context).primaryColor,
+          ),
+          title: Text(scans[i].valor),
+          subtitle: Text('Id: ${scans[i].id.toString()}'),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print('Hacer algo'),
+        ),
+      ),
+    );
+  }
+}
