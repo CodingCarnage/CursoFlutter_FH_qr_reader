6bb0b369e82eb84e702a3587a11d5c30c35427e2
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 21:17:05 -0600

    Cambio en UI y se agrego el metodo para lanzar los escaneos.

--- lib/widgets/scan_tiles.dart
+++ lib/widgets/scan_tiles.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/utils/utils.dart';
 
 class ScanTiles extends StatelessWidget {
   const ScanTiles({Key key, @required this.tipo}) : super(key: key);
@@ -19,7 +20,7 @@ class ScanTiles extends StatelessWidget {
       itemBuilder: (_, int i) => Dismissible(
         key: UniqueKey(),
         background: Container(
-          padding: const EdgeInsets.only(left: 5.0),
+          padding: const EdgeInsets.only(left: 10.0),
           color: Colors.red,
           alignment: Alignment.centerLeft,
           child: const Icon(Icons.delete, size: 30.0, color: Colors.white),
@@ -36,7 +37,7 @@ class ScanTiles extends StatelessWidget {
           title: Text(scans[i].valor),
           subtitle: Text('Id: ${scans[i].id.toString()}'),
           trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          onTap: () => print('Hacer algo'),
+          onTap: () => launchURL(context, scans[i]),
         ),
       ),
     );
