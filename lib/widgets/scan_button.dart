e434bb30ea2f59a6e02c602581dbef44553df636
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 22:03:47 -0600

    Cuando lee un QR ahora lanzara el launchURL.

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/utils/utils.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({Key key}) : super(key: key);
@@ -13,11 +14,15 @@ class ScanButton extends StatelessWidget {
       child: Icon(Icons.filter_center_focus),
       onPressed: () async {
         //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
-        const String barcodeScanRes = 'https://www.youtube.com';
+        const String barcodeScanRes = 'geo:26.909717,-101.423680';
         
+        if (barcodeScanRes == '-1')
+          return;
+
         final ScanListProvider scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
-        scanListProvider.newScan(barcodeScanRes);
-        scanListProvider.newScan('geo:26.909717,-101.423680');
+
+        final newScan = await scanListProvider.newScan(barcodeScanRes);
+        launchURL(context, newScan);
       },
     );
   }
