cb82fc4a14b359c488f6a45e37d2f79c998bbacb
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-28 22:45:22 -0600

    Cambio para prueba.

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -14,7 +14,7 @@ class ScanButton extends StatelessWidget {
       child: Icon(Icons.filter_center_focus),
       onPressed: () async {
         //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
-        const String barcodeScanRes = 'geo:26.909717,-101.423680';
+        const String barcodeScanRes = 'geo:26.925742,-101.447122';
         
         if (barcodeScanRes == '-1')
           return;
